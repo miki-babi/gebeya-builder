163a3078008d023beff7905bd3ab3edbed8126d0
miki-babi <mikiyasshiferaw@gmail.com>
2025-06-14 23:06:03 +0300

    remove csrf

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TelegramController;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::post('/telegram/webhook', [TelegramController::class, 'handleWebhook']);
+Route::post('/telegram/webhook', [TelegramController::class, 'handleWebhook'])->withoutMiddleware([VerifyCsrfToken::class]);
