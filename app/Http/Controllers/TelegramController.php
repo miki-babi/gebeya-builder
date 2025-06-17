505f3d2b872bf4c1dbf496ede5175fbe202fe01b
miki-babi <mikiyasshiferaw@gmail.com>
2025-06-17 10:27:24 +0300

    test

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -38,7 +38,7 @@ class TelegramController extends Controller
             '@axumverse',
             'Welcome! Check out our store.',
             'Open Store',
-            'https://t.me/gebeya_builderbot?start=startapp=wejkwjekw',
+            'https://t.me/gebeya_builderbot?startapp',
         );
     }
 
