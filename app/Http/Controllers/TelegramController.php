0b6e66927c52ec7ef795e44e603b9e227b421127
miki-babi <mikiyasshiferaw@gmail.com>
2025-06-17 15:11:59 +0300

    tetet

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -48,8 +48,8 @@ class TelegramController extends Controller
 ];
 
 $photoUrls = [
-    'https://images.pexels.com/photos/8597551/pexels-photo-8597551.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    'https://images.pexels.com/photos/8597551/pexels-photo-8597551.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/8597551/pexels-photo-8597551.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    // 'https://images.pexels.com/photos/8597551/pexels-photo-8597551.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 ];
 
 Telegram::sendProductPost($chatId, $photoUrls, $product);
